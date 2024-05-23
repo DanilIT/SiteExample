dcc8dbc574082b79c1678ec47e97888741f55907
Danil Shirkin <danil.shirkin@coral.ru>
2024-05-22 21:03:19 -0700

    basic infrastructure

--- TestSite/Startup.cs
+++ TestSite/Startup.cs
@@ -16,6 +16,9 @@ namespace TestSite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //adding mvc support
+            services.AddControllersWithViews();
+                   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +31,12 @@ namespace TestSite
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                                
             });
         }
     }
